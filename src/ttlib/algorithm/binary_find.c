e1a18eddbfcb56599606e0529aa335bc9e036947
tangaoo <tangaoo@126.com>
2020-12-04 21:30:10 +0800

    1. msvc切gcc，去warnning； 2.增加find algo； 3.改trace.h，兼容不同编译器。

---
+++ src/ttlib/algorithm/binary_find.c
@@ -0,0 +1,29 @@
+/*!The TT Library
+ *
+ * @Copyright (C) 2019-2020, TTLIB
+ *
+ * @file       binary_find.c
+ * @ingroup    algorithm
+ * @author     tango
+ * @date       2020-12-03 
+ * @brief      binary_find.c file
+ */
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "binary_find.h"
+#include "binary_find_if.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * implementation
+ */
+tt_size_t tt_binary_find(tt_iterator_ref_t iterator, tt_size_t head, tt_size_t tail, tt_cpointer_t item)
+{
+    return tt_binary_find_if(iterator, head, tail, tt_iterator_comp, item);
+}
+
+tt_size_t tt_binary_find_all(tt_iterator_ref_t iterator, tt_cpointer_t item)
+{
+    return tt_binary_find_all_if(iterator, tt_iterator_comp, item);
+}
