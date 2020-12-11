aeb55d431c90005d0dbca62f799464ebedeef620
tangaoo <tangaoo@126.com>
2020-12-11 22:24:56 +0800

    add find module

---
+++ src/ttlib/algorithm/find_if.c
@@ -0,0 +1,25 @@
+/*!The TT Library
+ *
+ * @Copyright (C) 2019-2020, TTLIB
+ *
+ * @file       find_if.c
+ * @ingroup    algorithm
+ * @author     tango
+ * @date       2020-12-11 
+ * @brief      find_if.c file
+ */
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "find_if.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * implementation
+ */
+
+tt_bool_t tt_find_if(tt_iterator_ref_t iterator, tt_size_t head, tt_size_t tail, tt_predicate_func_t pred, tt_cpointer_t data)
+{
+    // check 
+    
+}
---
+++ src/ttlib/algorithm/predicate.c
@@ -0,0 +1,66 @@
+/*!the tt library
+ *
+ * @copyright (c) 2019-2020, ttlib
+ *
+ * @file       predicate.c
+ * @ingroup    algorithm
+ * @author     tango
+ * @date       2020-12-11 
+ * @brief      predicate.c file
+ */
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "predicate.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * implementation
+ */
+
+tt_bool_t tt_predicate_eq(tt_iterator_ref_t iterator, tt_cpointer_t item, tt_cpointer_t data)
+{
+    // assert
+    tt_assert(iterator);
+    
+    // item == data?
+    return !tt_iterator_comp(iterator, item, data);
+}
+
+tt_bool_t tt_predicate_bq(tt_iterator_ref_t iterator, tt_cpointer_t item, tt_cpointer_t data)
+{
+    // assert
+    tt_assert(iterator);
+    
+    // item > data?
+    return tt_iterator_comp(iterator, item, data) > 0;
+}
+
+tt_bool_t tt_predicate_lq(tt_iterator_ref_t iterator, tt_cpointer_t item, tt_cpointer_t data)
+{
+    // assert
+    tt_assert(iterator);
+    
+    // item < data?
+    return tt_iterator_comp(iterator, item, data) < 0;
+}
+
+tt_bool_t tt_predicate_beq(tt_iterator_ref_t iterator, tt_cpointer_t item, tt_cpointer_t data)
+{
+    // assert
+    tt_assert(iterator);
+    
+    // item >= data?
+    return tt_iterator_comp(iterator, item, data) >= 0;
+}
+
+tt_bool_t tt_predicate_leq(tt_iterator_ref_t iterator, tt_cpointer_t item, tt_cpointer_t data)
+{
+    // assert
+    tt_assert(iterator);
+    
+    // item <= data?
+    return tt_iterator_comp(iterator, item, data) <= 0;
+}
+
+
