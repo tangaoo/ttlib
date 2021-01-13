f962b88abe3f868c0e1c6ce3b52687dee7ded598
tangaoo <tangaoo@126.com>
2021-01-13 22:07:16 +0800

    [add] add socket, doing

---
+++ src/ttlib/platform/socket.c
@@ -0,0 +1,10 @@
+/*!The TT Library
+ *
+ * @Copyright (C) 2019-2021, TTLIB
+ *
+ * @file       socket.c
+ * @ingroup    platform
+ * @author     tango
+ * @date       2021-01-13 
+ * @brief      socket.c file
+ */
