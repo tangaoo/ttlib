ab583dd54a63c05e5fdd1bffe827ab60ff8e1626
tangaoo <tangaoo@126.com>
2021-02-02 22:45:15 +0800

    add unixaddr.c done

--- src/ttlib/network/unixaddr.c
+++ src/ttlib/network/unixaddr.c
@@ -58,5 +58,21 @@ tt_char_t const* tt_unixaddr_cstr(tt_unixaddr_ref_t unixaddr, tt_char_t* data, t
     // check
     tt_assert_and_check_return_val(unixaddr && data && maxn >= TT_UNIXADDR_CSTR_MAXN, tt_null);
 
+    // done
+    tt_size_t size = snprintf(data, maxn - 1, "%s", unixaddr->path);
+    if (size >=0) data[size] = '\0';
 
+    // ok
+    return data;
+}
+
+tt_bool_t tt_unixaddr_str_set(tt_unixaddr_ref_t unixaddr, tt_char_t const* cstr, tt_bool_t is_abstract)
+{
+    // check
+    tt_assert_and_check_return_val(unixaddr && cstr, tt_false);
+
+    // set abstract
+    unixaddr->is_abstract = is_abstract;
+
+    return strncpy(unixaddr->path, cstr, TT_UNIXADDR_CSTR_MAXN) < TT_UNIXADDR_CSTR_MAXN;
 }
