27ed68e2d5cdae2ab48acd4a060b571b67af3417
tangaoo <tangaoo@126.com>
2021-03-27 22:27:44 +0800

    add spinlock2

---
+++ src/ttlib/platform/spinlock2.c
@@ -0,0 +1,15 @@
+/*!The TT Library
+ *
+ * @Copyright (C) 2019-2021, TTLIB
+ *
+ * @file       spinlock2.c
+ * @ingroup    platform
+ * @author     tango
+ * @date       2021-03-27 
+ * @brief      spinlock2.c file
+ */
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "spinlock2.h"
