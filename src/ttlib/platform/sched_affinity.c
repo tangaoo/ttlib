5653a191a3bed6d1f1a246922f733ad2bb68d454
tangaoo <tangaoo@126.com>
2021-05-06 20:55:29 +0800

    add sched_affinity

---
+++ src/ttlib/platform/sched_affinity.c
@@ -0,0 +1,10 @@
+/*!The TT Library
+ *
+ * @Copyright (C) 2019-2021, TTLIB
+ *
+ * @file       sched_affinity.c
+ * @ingroup    platform
+ * @author     tango
+ * @date       2021-05-06 
+ * @brief      sched_affinity.c file
+ */
---
+++ src/ttlib/platform/sched_affinity.h
@@ -0,0 +1,17 @@
+/*!The TT Library
+ *
+ * @Copyright (C) 2019-2021, TTLIB
+ *
+ * @file       sched_affinity.h
+ * @ingroup    platform
+ * @author     tango
+ * @date       2021-05-06 
+ * @brief      sched_affinity.h file
+ */
+
+#ifndef TT_SCHED_AFFINITY_H
+#define TT_SCHED_AFFINITY_H
+
+
+
+#endif
