c70a4fa2f2a25361efc88834a16651ef00957e4e
tangaoo <tangaoo@126.com>
2021-05-12 21:29:31 +0800

    change

--- src/ttlib/platform/sched_affinity.c
+++ src/ttlib/platform/sched_affinity.c
@@ -14,3 +14,24 @@
  */
 #include "sched_affinity.h"
 
+int attach_cpu(int cpu_index)
+{
+    int cpu_num = sysconf(_SC_NPROCESSORS_CONF);
+    if (cpu_index < 0 || cpu_index >= cpu_num)
+    {
+        printf("cpu index ERROR!\n");
+        return -1;
+    }
+
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(cpu_index, &mask);
+
+    if (pthread_setaffinity_np(pthread_self(), sizeof(mask), &mask) < 0)
+    {
+        printf("set affinity np ERROR!\n");
+        return -1;
+    }
+
+    return 0;
+}
